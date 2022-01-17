ae551a53872e5c17f81150657c9b5154fc015590
Patrick <contato.patrickbarreto@gmail.com>
2022-01-17 19:19:34 -0300

    x

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
     require_once './classes/usuarios.php';
     $usuario = new Usuario();
     require_once './classes/privado.php';
-    $host = new host();
+    $host = new host;
 ?>
 <?php
         if(isset($_POST["email"])){
             $email= addslashes($_POST["email"]);
             $senha= addslashes($_POST["senha"]);
             if(!empty($email) && !empty($senha)){
-                //*USAR EM LOCALHOST $usuario->conectar("Cadastro", "localhost", "root", $host->getSenha("localhost"));
-                /*USAR NO HOST*/ $usuario->conectar("immobe65_Cadastro", "localhost", "immobe65_patrick", $host->getSenha("host"));
-               if($usuario->msgErro == ""){
+                //USAR EM LOCALHOST $usuario->conectar("Cadastro", "localhost", "root", $host->getSenha("localhost"));
+                /*USAR NO HOST*/ $usuario->conectar("immobe65_Cadastro", "localhost", "immobe65_patrick", $host->getSenha("host"));      
+        if($usuario->msgErro == ""){
                 echo $usuario->msgErro;
                     if($usuario->logar($email, $senha)){
                         header("location: ./area_privada/curriculum.php");
